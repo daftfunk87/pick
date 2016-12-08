1843f7bae780eee7233660e9f34d58eb88017bff
daftfunk87 <jrstuart@live.com>
2016-12-08 15:00:14 -0700

    Update bot_read.py

--- RedditBot-master/Part1/bot_read.py
+++ RedditBot-master/Part1/bot_read.py
@@ -5,10 +5,10 @@ user_agent = ("PyEng Bot 0.1")
 
 r = praw.Reddit(user_agent = user_agent)
 
-subreddit = r.get_subreddit("learnpython")
+subreddit = r.get_subreddit("r/calgaryflames")
 
 for submission in subreddit.get_hot(limit = 5):
-    print "Title: ", submission.title
-    print "Text: ", submission.selftext
-    print "Score: ", submission.score
-    print "---------------------------------\n"
+    print ("Title: "), submission.title
+    print ("Text: "), submission.selftext
+    print ("Score: "), submission.score
+    print ("---------------------------------\n")
